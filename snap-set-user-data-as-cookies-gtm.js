65d4d91c0aa7da2c29e51eb394d4aad90c41ebea
Armin <kzardkoohi@gmail.com>
2021-12-15 15:42:14 -0600

    WIP: refactoring functions to use config.json file

--- snap-set-user-data-as-cookies-gtm.js
+++ snap-set-user-data-as-cookies-gtm.js
@@ -2,9 +2,9 @@
  * Start Helper Functions
  */
 
-if (typeOf(setCookie) !== "function") { // this function might already be set in another javascript file
+if (typeOf(setCookie) !== "function") { // this function might already be set in another javascript file so we check it here
   function setCookie(variableName, variableValue, expirationInDays) {
-    // console.log("Running function setCookie(" + variableName + ", " + variableValue + ", " + expirationInDays + ")");
+    console.log("Running function setCookie(" + variableName + ", " + variableValue + ", " + expirationInDays + ")");
     var domain = ".snapfinance.com";
     var path = "/";
     if (expirationInDays === null) {
@@ -21,6 +21,24 @@ if (typeOf(setCookie) !== "function") { // this function might already be set in
     document.cookie = cookieStringLastUpdated;
   }
 }
+
+if (typeOf(readTextFile) !== "function") { // this function might already be set in another javascript file so we check it here
+  function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            return rawFile.responseText;
+        }
+    }
+    rawFile.send(null);
+  }
+}
+//usage:
+// https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
+// var fileText = readTextFile("https://mmsfpr.github.io/config.json");
+
 
 function addClassToElement(querySelector, className) {
   var element = document.querySelector(querySelector);
@@ -47,6 +65,7 @@ function removeClassFromElement(querySelector, className) {
 }
 
 function scanAllFormButtonsAndSetCookieValues() {
+  // TODO: replace this with reading config.json file
   var formButtonsObject = {
     "rentOrOwn": [
       "rentbtn",
@@ -68,43 +87,64 @@ function scanAllFormButtonsAndSetCookieValues() {
     for (var ii = 0; ii < formButtonFieldIds.length; ii++) {
       if (document.querySelector("#" + formButtonFieldIds[ii] + ".activeButtonListener.active") !== null) {
         formButtonHasSelection = true;
-        // console.log(formButtonObjectKeyName + ": " + formButtonFieldIds[ii]);
+        console.log(formButtonObjectKeyName + ": " + formButtonFieldIds[ii]);
       }
     }
     if (!formButtonHasSelection) {
-      // console.log(formButtonObjectKeyName + ": (none)");
+      console.log(formButtonObjectKeyName + ": (none)");
     }
   }
 }
 
 function addListenersToFormButtons() {
-  var formButtonsObject = {
-    "rentOrOwn": [
-      "rentbtn",
-      "ownbtn",
-    ],
-    "yearsAtThisAddress": [
-      "lessthanonebtn",
-      "onetothreebtn",
-      "threetofivebtn",
-      "fiveormorebtn",
-    ]
-  };
+  var configUrl = "https://mmsfpr.github.io/config.json";
+  var configText = readTextFile(configUrl);
+  var config = JSON.parse(configText);
+  var formButtons = config["formButtons"];
 
-  var formButtonsObjectKeys = Object.keys(formButtonsObject);
-  for (var i = 0; i < formButtonsObjectKeys.length; i++) {
-    var formButtonObjectKeyName = formButtonsObjectKeys[i];
-    var formButtonObjectKeyIds = formButtonsObject[formButtonObjectKeyName];
-    for (var ii = 0; ii < formButtonObjectKeyIds.length; ii++) {
-      var formButton = document.getElementById(formButtonObjectKeyIds[ii]);
-      if (formButton !== null) {
-        addClassToElement("#"+ formButtonObjectKeyIds[ii], "activeButtonListener");
-        formButton.addEventListener("focusout", function(event) {
-          scanAllFormButtonsAndSetCookieValues();
-        });
-      }
+  for (var i = 0; i < formButtons.length; i++) {
+    var formButtonObject = formButtons[i];
+    var elementAttributeName = formButtonObject["attributeName"];
+    var elementAttributeValue = formButtonObject["attributeValue"];
+    var elementQuerySelector = "[" + elementAttributeName + "='" + elementAttributeValue +"']";
+    var formButtonField = document.querySelector(elementQuerySelector);
+    if (formButtonField !== null) {
+      console.log("Adding listener for element matching querySelector(" + elementQuerySelector + ")");
+      addClassToElement(elementQuerySelector, "activeButtonListener");
+      formButtonField.addEventListener("focusout", function(event) {
+        scanAllFormButtonsAndSetCookieValues();
+      });
     }
   }
+  // var formButtonsObject = {
+  //   "rentOrOwn": [
+  //     "rentbtn",
+  //     "ownbtn",
+  //   ],
+  //   "yearsAtThisAddress": [
+  //     "lessthanonebtn",
+  //     "onetothreebtn",
+  //     "threetofivebtn",
+  //     "fiveormorebtn",
+  //   ]
+  // };
+
+  // // formButtonsObject = config["formButtons"];
+
+  // var formButtonsObjectKeys = Object.keys(formButtonsObject);
+  // for (var i = 0; i < formButtonsObjectKeys.length; i++) {
+  //   var formButtonObjectKeyName = formButtonsObjectKeys[i];
+  //   var formButtonObjectKeyIds = formButtonsObject[formButtonObjectKeyName];
+  //   for (var ii = 0; ii < formButtonObjectKeyIds.length; ii++) {
+  //     var formButton = document.getElementById(formButtonObjectKeyIds[ii]);
+  //     if (formButton !== null) {
+  //       addClassToElement("#"+ formButtonObjectKeyIds[ii], "activeButtonListener");
+  //       formButton.addEventListener("focusout", function(event) {
+  //         scanAllFormButtonsAndSetCookieValues();
+  //       });
+  //     }
+  //   }
+  // }
 }
 
 function addListenersForFormInputFields() {
@@ -131,12 +171,12 @@ function addListenersForFormInputFields() {
     var elementQuerySelector = "[" + elementAttributeName + "='" + elementAttributeValue +"']";
     var formInputField = document.querySelector(elementQuerySelector);
     if (formInputField !== null) {
-      // console.log("Adding listener for element matching querySelector(" + elementQuerySelector + ")");
+      console.log("Adding listener for element matching querySelector(" + elementQuerySelector + ")");
       formInputField.addEventListener("focusout", function(event) {
         var eventElementId = event.target.id;
         var eventElementName = event.target.name;
         var eventElementValue = event.target.value;
-        // console.log("eventElementId: " + eventElementId + " | eventElementName: " + eventElementName + " | eventElementValue: " + eventElementValue);
+        console.log("eventElementId: " + eventElementId + " | eventElementName: " + eventElementName + " | eventElementValue: " + eventElementValue);
         setCookie(eventElementName, eventElementValue, null);
       });
     }
